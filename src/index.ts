5587ba6fd2c0f8df86c8812996e02ab053557de9
灀洲 <shuangzhou.wh@alibaba-inc.com>
2020-10-20 16:57:43 +0800

    net program

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import net from 'net';
+import { log } from 'console';
+
+const sever = net.createServer((socket) => {
+    // 新的连接
+    socket.on('data', (data) => {
+        socket.write('你好');
+    });
+
+    socket.on('end', () => {
+        log('连接断开');
+    });
+
+    socket.write("欢迎光临灀洲的node服务测试示例: \n");
+});
+
+sever.listen(8001, () => {
+    log('server bound');
+});
